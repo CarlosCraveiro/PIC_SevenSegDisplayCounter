e33e9fab6e3183aac44064ef9994ebe6464d970b
Carlos Craveiro <CarlosCraveiro@usp.br>
2023-12-17 03:40:18 -0300

    adds a development shell

--- flake.nix
+++ flake.nix
@@ -3,8 +3,7 @@
   inputs = { 
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable"; 
     carlospkgs.url = "github:CarlosCraveiro/nixpkgs/master";
-    #flake-utils.url = "github:numtide/flake-utils";
-    };
+  };
 
   outputs = { self, nixpkgs, carlospkgs, ... }:
     let
@@ -22,7 +21,7 @@
             overlays = [ sdcc-overlay carlospkgs-overlay ];
 
         };
-                counterpic = with import nixpkgs { system = "x86_64-linux"; };  stdenv.mkDerivation {
+        counterpic = with import nixpkgs { system = "x86_64-linux"; };  stdenv.mkDerivation {
             name = "counterpic";
             src = self;
             
@@ -39,42 +38,21 @@
 
             buildPhase = ''
                 make all
-                echo "#!/bin/sh" > counterpic
-                echo "echo $out" >> counterpic
-                chmod +x counterpic
             '';
 
-                #echo "ls && $out/bin/simulide $out/simu/circuito.simu" >> counterpic
             installPhase = ''
                 mkdir -p $out
                 mkdir -p $out/simu
                 mkdir -p $out/bin
                 cp simu/circuito.simu $out/simu
-                cp counterpic $out/bin
                 cp main.hex $out
-            '';
-            #meta.mainProgram = "counterpic";
+            ''; 
         };
     in {
       
-        #devShells.${system}.default = import ./shell.nix { inherit pkgs; };
-                packages.${system} = { 
-
-/*
-    default = pkgs.writeShellApplication {
-        name = "counterpic";
-        runtimeInputs = with pkgs; [ 
-            gputils
-            sdcc
-            simulide
-            gnumake
-        ];
-        text = ''
-          cd $out && make all && simulide simu/circuito.simu
-        '';
-      };*/
-
-        default = pkgs.symlinkJoin {
+        devShells.${system}.default = import ./shell.nix { inherit pkgs; };
+        packages.${system} = { 
+            default = pkgs.symlinkJoin {
             name = "simulation";
             paths = with pkgs; [ counterpic pkgs.simulide ];
             postBuild = ''
